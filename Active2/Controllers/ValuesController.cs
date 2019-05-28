3b3565c6aa053ba1e6c316195ceb684e77de2e29
Milán Jakab Gulyás <gumija6@gmail.com>
2019-05-28 11:48:49 +0200

    WebApi

---
+++ Active2/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Active2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        bool active = true;
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<bool> Get()
+        {
+            return active;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] bool value)
+        {
+            active = value;
+        }
+    }
+}
