f5750ae6e81d516f9bead7ebbc283391a69289ad
Richard Borcsik <borcsikrichard@gmail.com>
2019-05-28 12:33:08 +0200

    function: Get response and send email

--- Function/Function1.cs
+++ Function/Function1.cs
@@ -1,16 +1,61 @@
 using System;
+using System.Net;
+using System.Net.Http;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using PostmarkDotNet;
+using PostmarkDotNet.Model;
+
 
 namespace Function
 {
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static void Run([TimerTrigger("0 */ 1 * ***")]TimerInfo myTimer, ILogger log)
+
+        public  static async void Run([TimerTrigger("0 * * * * *")] TimerInfo myTimer, ILogger log)
         {
+
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        
+        HttpClient client = new HttpClient();
+
+
+            var res =  client.GetAsync("http://active2.azurewebsites.net/api/values");
+            
+            string activeFlag = await res.Result.Content.ReadAsStringAsync();
+
+            log.LogInformation($"Message was {activeFlag}");
+
+            if (activeFlag == "true")
+            {
+
+                var message = new PostmarkMessage()
+                {
+                    To = "support@happyatwork.se",
+                    From = "info@happyatwork.se",
+                    Subject = "Coding exercise",
+                    TextBody = "Hello!",
+                };
+
+                string postmarkToken = Environment.GetEnvironmentVariable("POSTMARK_TOKEN");
+
+                var pmClient = new PostmarkClient(postmarkToken);
+                var sendResult = await pmClient.SendMessageAsync(message);
+
+                if (sendResult.Status == PostmarkStatus.Success) { log.LogInformation("Success"); }
+                else
+                {
+                    log.LogError("Error occured");
+                    log.LogError($"{sendResult.ErrorCode}");
+                    log.LogError($"{sendResult.Message}");
+                }
+
+            }
+
         }
+
     }
+
 }
